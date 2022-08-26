c20eae0363f54584eafa5013b2defb58f0a9db2d
subbus <subbareddyg83@gmail.com>
2022-08-26 08:46:57 +0530

    add  print regular triangle

---
+++ 03-recursion/print-regular-traingle-recursive.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <assert.h>
+using namespace std;
+void print_triangle(int n)
+{
+	if (n == 0)
+	{
+		return;
+	}
+	print_triangle(n - 1);
+	for (int i = 0; i < n; i++)
+	{
+		cout << '*';
+	}
+	cout << endl;
+}
+int main()
+{
+	// comment these 2 lines for console I/O rather than file I/O
+	freopen("..\\00-in.txt", "r", stdin);
+	freopen("..\\00-out.txt", "w", stdout);
+
+	int n;
+	cin >> n;
+	print_triangle(n);
+
+	return 0;
+}
